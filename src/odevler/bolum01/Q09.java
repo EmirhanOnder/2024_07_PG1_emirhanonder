c87d081bfd1fbf47c1216fe4039f0efe92417f8e
emirhan <ondremirhan@gmail.com>
2024-08-11 16:33:06 +0300

    Chapter-1 9th question update

---
+++ src/odevler/bolum01/Q09.java
@@ -0,0 +1,15 @@
+package odevler.bolum01;
+
+public class Q09 {
+    public static void main(String[] args) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        double perimeter = 2*(width + height);
+        double area = width * height;
+
+        System.out.println("Perimeter is: " + perimeter);
+        System.out.println("Area is: " + area);
+    }
+}
