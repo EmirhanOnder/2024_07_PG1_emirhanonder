cde97da37a412868a9f031b98872254297364ea9
emirhan <ondremirhan@gmail.com>
2024-08-11 16:31:16 +0300

    Chapter-1 8th question update

---
+++ src/odevler/bolum01/Q08.java
@@ -0,0 +1,18 @@
+package odevler.bolum01;
+
+public class Q08 {
+    public static void main(String[] args) {
+
+        double pi = 3.14159265358979;
+        double radius = 5.5;
+
+        double perimeter;
+        double area;
+
+        perimeter = 2*radius*pi;
+        area = radius*radius*pi;
+
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+}
