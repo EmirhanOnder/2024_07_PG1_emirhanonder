f7c608c524d4348f2829d83c8ac13bc548772b07
emirhan <ondremirhan@gmail.com>
2024-08-11 15:56:33 +0300

    Chapter-1 2nd question update

--- src/odevler/bolum01/Q02.java
+++ src/odevler/bolum01/Q02.java
@@ -1,4 +1,10 @@
 package odevler.bolum01;
 
 public class Q02 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Welcome to Java\n");
+        }
+    }
 }
