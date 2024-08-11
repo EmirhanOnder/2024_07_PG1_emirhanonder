c6e25172481815e0af6734f05db906fe12fb30b4
emirhan <ondremirhan@gmail.com>
2024-08-11 16:27:39 +0300

    Chapter-1 6th question update

---
+++ src/odevler/bolum01/Q07.java
@@ -0,0 +1,18 @@
+package odevler.bolum01;
+
+public class Q07 {
+    public static void main(String[] args) {
+
+        double result;
+
+        result = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+
+        System.out.println(result);
+
+        double second_result;
+
+        second_result = 4 * (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11) + (1.0/13));
+
+        System.out.println(second_result);
+    }
+}
