fb575e1d4124fba73eac233c3ef1d6e803b596fb
emirhan <ondremirhan@gmail.com>
2024-08-11 16:21:54 +0300

    Chapter-1 6th question update

---
+++ src/odevler/bolum01/Q06.java
@@ -0,0 +1,17 @@
+package odevler.bolum01;
+
+public class Q06 {
+    public static void main(String[] args) {
+        System.out.println(1+2+3+4+5+6+7+8+9);
+
+        /*
+        int result = 0;
+
+        for(int i = 1;i<10;i++)
+        {
+            result += i;
+        }
+        System.out.println(result);
+        */
+    }
+}
